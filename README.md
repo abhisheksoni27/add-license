34e574ec88e629293d407053a0a9c70fa3a4c9d0
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-04-02 19:03:39 +0530

    RELEASE 0.0.1 :tada:

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # add-license
-A CL utility to add license text to all your JS files.
+A CL utility to add `License` text to all your **JS** files.
 
 ## Table of contents
 
@@ -8,3 +8,55 @@ A CL utility to add license text to all your JS files.
 * [Legal Stuff](#legal-stuff)
 
 ## Installation
+
+We love yarn. And we are using **global** because it'd be `stupid` to add it to every project, if it's only going to be used at the dev's machine, and that too, only once.
+
+
+```bash
+$ yarn add global add-license
+```
+
+If you like npm, do this:
+
+```bash
+$ npm install -g add-license
+```
+This adds the `add-license` command to your path, so that you can use it everywhere. Wicked, I know!
+
+## Usage
+
+Pretty Straight Forward: Navigate to any project that has a package.json file, and run the following command:
+
+```bash
+$ add-license [license-name] [author-name]
+```
+
+**`license-name`**: Yes. That's it. MIT, or Apache.
+
+**`author-name`**: Whatsoever you wanna call yourself.
+
+This will add the License Text to all the JS files in your project. :tada:
+
+## Legal Stuff
+
+MIT License
+
+Copyright (c) 2017 &ndash; eternity Abhishek Soni
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
+/**
+* MIT License
+*
+* Copyright (c) 2017 Abhishek Soni
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+
 const find = require('find');
 
 const prepend = require('prepend-file');
@@ -8,23 +33,39 @@ const removeNodeModules = require('./util/removeNodeModules.js');
 
 const getLicense = require('./licenses/getLicense.js');
 
+const conArgv = ['mit', 'Anonymous'];
+
+let argv = process
+    .argv
+    .slice(2);
+
+if (argv.length === 0) {
+    argv = conArgv;
+}
+
+const license = argv[0];
+
+const name = argv.slice(1).join(' ');
+
 find.file(/\package.json$/, process.cwd(), (files) => {
 
     let packageJson = removeNodeModules(files);
 
-    if(!packageJson) return;
-
+    if (!packageJson) 
+        return;
+    
     findJSFiles();
 });
 
-findJSFiles = ()=>{
-    find.file(/\.js$/, process.cwd(), (files)=>{
+const licenseText = getLicense(license, name);
+
+findJSFiles = () => {
+    find.file(/\.js$/, process.cwd(), (files) => {
         let JSfiles = removeNodeModules(files);
-        JSfiles.forEach((fileName)=>{
-            prepend(fileName,getLicense('mit', 'Fantastic Waddle'),(err)=>{
+        JSfiles.forEach((fileName) => {
+            prepend(fileName, licenseText, (err) => {
                 console.log(err);
             })
         });
     })
 }
-
